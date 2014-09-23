8ca7f06aedd38b1ee7abc2846da5487e4cf19750
Linda <lindali@Lindas-MacBook-Pro.local>
2014-09-23 01:33:25 -0400

    added google analytics

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 $(".title-down").click(function(){
     $("body").animate({scrollTop: $(window).height()}, 500);
+    ga('send', 'event', 'button', 'click', 'title-down');
 });
+
+$(".p1").click(function() {
+   ga('send', 'event', 'button', 'click', 'p1');
+});
+
+$(".p2").click(function() {
+   ga('send', 'event', 'button', 'click', 'p2');
+});
+
+$(".p3").click(function() {
+   ga('send', 'event', 'button', 'click', 'p3');
+});
+
+$(".p4").click(function() {
+   ga('send', 'event', 'button', 'click', 'p4');
+});
+
+$(".fa-linkedin-square").click(function() {
+	ga('send', 'event', 'button', 'click', 'linkedIn');
+})
+
+$(".fa-github-square").click(function() {
+	ga('send', 'event', 'button', 'click', 'github');
+})
+
+$(".fa-twitter-square").click(function() {
+	ga('send', 'event', 'button', 'click', 'twitter');
+})
+
+$(".fa-mail-square").click(function() {
+	ga('send', 'event', 'button', 'click', 'mail');
+})
