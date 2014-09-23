66537b0e56416b7f92458922bcb23f50e18a9bd2
Linda Li <lxl@andrew.cmu.edu>
2014-09-23 02:05:08 -0400

    qq im dum

--- script.js
+++ script.js
@@ -3,34 +3,6 @@ $(".title-down").click(function(){
     ga('send', 'event', 'button', 'click', 'title-down');
 });
 
-$(".p1").click(function() {
-   ga('send', 'event', 'button', 'click', 'p1');
+$('a').click(function() {
+  ga('send', 'event', 'link', 'click', $(this).attr('href'));
 });
-
-$(".p2").click(function() {
-   ga('send', 'event', 'button', 'click', 'p2');
-});
-
-$(".p3").click(function() {
-   ga('send', 'event', 'button', 'click', 'p3');
-});
-
-$(".p4").click(function() {
-   ga('send', 'event', 'button', 'click', 'p4');
-});
-
-$(".fa-linkedin-square").click(function() {
-	ga('send', 'event', 'button', 'click', 'linkedIn');
-})
-
-$(".fa-github-square").click(function() {
-	ga('send', 'event', 'button', 'click', 'github');
-})
-
-$(".fa-twitter-square").click(function() {
-	ga('send', 'event', 'button', 'click', 'twitter');
-})
-
-$(".fa-mail-square").click(function() {
-	ga('send', 'event', 'button', 'click', 'mail');
-})
